f443ed35885a071eaa04c0f819c64fedca13b0ee
salome mexrishvili <salomemexrishvili@salomes-MacBook-Air.local>
2026-05-08 11:45:06 +0400

    fix login button selector

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,6 +2,6 @@ Cypress.Commands.add('login', (username, password) => {
   cy.visit('https://automationteststore.com/index.php?rt=account/login')
   cy.get('#loginFrm_loginname').clear().type(username)
   cy.get('#loginFrm_password').clear().type(password)
-  cy.get('button[title="Login"]').click()
+  cy.get('button[type="submit"]').first().click()
   cy.url().should('include', 'account/account')
 })
