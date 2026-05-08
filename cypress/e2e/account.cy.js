0b07fa7274a4d3b099138a3ca4fdcc5e89187b01
salome mexrishvili <salomemexrishvili@salomes-MacBook-Air.local>
2026-05-09 01:08:59 +0400

    add account management tests

--- cypress/e2e/account.cy.js
+++ cypress/e2e/account.cy.js
@@ -1,8 +1,9 @@
 describe('Account Management', () => {
+  // Login before each test
   beforeEach(() => {
     cy.login('salome', 'salome02042003')
   })
-  it('should edit account details and verify changes are saved', () => {
+  it('edits account details and saves', () => {
     cy.visit('https://automationteststore.com/index.php?rt=account/edit')
     cy.get('#AccountFrm_firstname').clear().type('Salome')
     cy.get('#AccountFrm_lastname').clear().type('Mekrishvili')
@@ -12,28 +13,31 @@ describe('Account Management', () => {
     cy.get('#AccountFrm_firstname').should('have.value', 'Salome')
     cy.get('#AccountFrm_lastname').should('have.value', 'Mekrishvili')
   })
-  it('should edit address', () => {
+  it('edits address and saves', () => {
     cy.visit('https://automationteststore.com/index.php?rt=account/address')
-    cy.get('a[href*="address"]').first().click({force: true})
+    cy.contains('New Address').click()
     cy.get('#AddressFrm_firstname').clear().type('Salome')
     cy.get('#AddressFrm_lastname').clear().type('Mekrishvili')
-    cy.get('#AddressFrm_address_1').clear().type('123 Test Street')
+    cy.get('#AddressFrm_address_1').clear().type('Kedia 14')
     cy.get('#AddressFrm_city').clear().type('Tbilisi')
+    cy.get('#AddressFrm_postcode').clear().type('0119')
+    cy.get('#AddressFrm_country_id').select('Georgia')
+    cy.get('#AddressFrm_zone_id').select('Imereti')
     cy.get('button[title="Continue"]').click()
-    cy.contains('Your address has been successfully').should('be.visible')
+    cy.contains('Your address has been successfully inserted').should('be.visible')
   })
-  it('should change password', () => {
+  it('changes password and reverts', () => {
     cy.visit('https://automationteststore.com/index.php?rt=account/password')
     cy.get('input[name="current_password"]').type('salome02042003')
-    cy.get('input[name="password"]').type('NewPassword123')
-    cy.get('input[name="confirm"]').type('NewPassword123')
+    cy.get('input[name="password"]').type('salo12')
+    cy.get('input[name="confirm"]').type('salo12')
     cy.get('button[title="Continue"]').click()
     cy.contains('Your password has been successfully updated.').should('be.visible')
     cy.visit('https://automationteststore.com/index.php?rt=account/logout')
-    cy.login('salome', 'NewPassword123')
+    cy.login('salome', 'salo12')
     cy.url().should('include', 'account/account')
     cy.visit('https://automationteststore.com/index.php?rt=account/password')
-    cy.get('input[name="current_password"]').type('NewPassword123')
+    cy.get('input[name="current_password"]').type('salo12')
     cy.get('input[name="password"]').type('salome02042003')
     cy.get('input[name="confirm"]').type('salome02042003')
     cy.get('button[title="Continue"]').click()
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,7 +1,7 @@
 Cypress.Commands.add('login', (username, password) => {
   cy.visit('https://automationteststore.com/index.php?rt=account/login')
-  cy.get('#loginFrm_loginname').clear().type(username)
-  cy.get('#loginFrm_password').clear().type(password)
-  cy.get('button[type="submit"]').first().click()
-  cy.url().should('include', 'account/account')
+  cy.get('#loginFrm_loginname').should('be.visible').clear().type(username, { delay: 100 })
+  cy.get('#loginFrm_password').should('be.visible').clear().type(password, { delay: 100 })
+  cy.get('#loginFrm').find('button[type="submit"]').click()
+  cy.url({ timeout: 15000 }).should('include', 'account/account')
 })
